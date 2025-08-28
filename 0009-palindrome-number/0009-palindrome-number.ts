93580b8b5372f92dcd797aee346c87754449072f
Obaloluwa Tubi <99616530+TubiOb@users.noreply.github.com>
2025-08-28 20:20:31 +0100

    Time: 8 ms (48.58%), Space: 66 MB (15.13%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.ts
+++ 0009-palindrome-number/0009-palindrome-number.ts
@@ -1,23 +1,8 @@
 function isPalindrome(x: number): boolean {
     let nums = '';
     let numStr = String(x);
-    console.log(numStr)
-    // for (let i = 0; i < numStr.length; i++) {
-    //     console.log('Before Pop', numStr)
-    //     nums += numStr.pop();
-    //     console.log('After Pop', numStr)
-    // }
-    // console.log(nums)
-    // if (Number(nums) === x) {
-    //     return true;
-    // }
-    // else {
-    //     return false;
-    // }
     for (let i = numStr.length - 1; i >= 0; i--) {
         nums += numStr[i];
-        console.log(numStr.length)
     }
-    console.log(nums)
     return Number(nums) === x;
 };
